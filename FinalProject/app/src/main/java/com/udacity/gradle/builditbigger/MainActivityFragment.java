fdc822431f32cbe3045e9147ae044a44c3033097
Angelo Rüggeberg <s3xy4ngyc@googlemail.com>
2016-06-28 16:12:48 +0200

    Updated Buildtools Versions and Target SDK to newest, Updated Admob to Firebase Ads, Added First Drop of Java Library Providing Jokes

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,19 +1,32 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.s3xy.CaptainFunny;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    @BindView(R.id.adView)
+    AdView mAdView;
+
+    @Nullable
+    private Toast mToast;
 
     public MainActivityFragment() {
     }
@@ -23,14 +36,29 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-        AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        ButterKnife.bind(this, root);
+
+//        // Create an ad request. Check logcat output for the hashed device ID to
+//        // get test ads on a physical device. e.g.
+//        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+
         return root;
     }
+
+
+    @OnClick(R.id.btnTellJoke)
+    void tellJoke() {
+        if (mToast != null) {
+            mToast.cancel();
+        }
+
+        mToast = Toast.makeText(getContext(), CaptainFunny.tellJoke(), Toast.LENGTH_SHORT);
+        mToast.show();
+
+    }
+
 }
---
+++ FinalProject/captainfunny/src/main/java/de/s3xy/CaptainFunny.java
@@ -0,0 +1,7 @@
+package de.s3xy;
+
+public class CaptainFunny {
+    public static String tellJoke() {
+        return "What do computers and air conditioners have in common? They both become useless when you open windows";
+    }
+}
